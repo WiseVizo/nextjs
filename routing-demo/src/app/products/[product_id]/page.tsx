f8be68180d62e86c37358d9d05588df5b2047a9e
WiseVizo <mecoc1011@gmail.com>
2024-11-15 09:58:17 +0530

    dynamic routing

---
+++ routing-demo/src/app/products/[product_id]/page.tsx
@@ -0,0 +1,3 @@
+export default function productDetails({params}){
+    return <h1>product details of {params.product_id}</h1>
+}
---
+++ routing-demo/src/app/products/page.tsx
@@ -0,0 +1,8 @@
+export default function products(){
+    return <>
+            <h1>Product 1</h1>
+            <h1>Product 2</h1>
+            <h1>Product 3</h1>
+            <h1>Product 4</h1>
+    </>
+}
