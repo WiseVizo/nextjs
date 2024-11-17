1c4c5bd23ae775e601ab49e9ef5f7977ca40bf60
WiseVizo <mecoc1011@gmail.com>
2024-11-17 09:45:40 +0530

    learning with gpt

---
+++ my-potfolio/src/app/layout.js
@@ -0,0 +1,11 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <h1>Welcome</h1>
+        {children}
+        <h6>Cya</h6>
+      </body>
+    </html>
+  );
+}
---
+++ my-potfolio/src/app/page.js
@@ -0,0 +1,3 @@
+export default function HomePage(){
+  return <h1>Hlo this is my potfolio</h1>
+}
