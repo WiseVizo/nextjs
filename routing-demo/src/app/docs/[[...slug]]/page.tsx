6f08b9891ab00a0bd5a30f734040994873de69dc
WiseVizo <mecoc1011@gmail.com>
2024-11-20 20:59:43 +0530

    using [[...slug]] for catching all urls

--- routing-demo/src/app/docs/[[...slug]]/page.tsx
+++ routing-demo/src/app/docs/[[...slug]]/page.tsx
@@ -1,9 +1,9 @@
 export default async function DocsPage({params}:{params: {slug: string[]}}){
     const parameters = await params
-    if (parameters.slug.length==2){
+    if (parameters.slug?.length==2){
         return <h1>Reading docs feature {parameters.slug[0]} and concept {parameters.slug[1]}</h1>
     }
-    else if (parameters.slug.length==3){
+    else if (parameters.slug?.length==3){
         return <h1>Reading docs feature {parameters.slug[0]} and concept {parameters.slug[1]} example {parameters.slug[2]}</h1>
     }
     else{
